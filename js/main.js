5668d7d2d871b498b7738b71a01af7ec89fdc52d
yungromantic <78252352+yungromantic@users.noreply.github.com>
2022-02-01 20:25:14 +0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,134 @@
+window.addEventListener("DOMContentLoaded", () => {
+  const body = document.querySelector("body"),
+        windowWidth = window.outerWidth,
+        burger = document.querySelector('.burger'),
+        hero = document.querySelector('.hero'),
+        catalogCards = document.querySelectorAll('.catalog__card'),
+        galleryBtn = document.querySelector('.gallery__btn'),
+        modalAbout = document.querySelector('.modal__about-wrapper');
+
+  function removeBodyClass() {
+    setTimeout(() => {
+      body.classList.remove("lock");
+    }, 4300);
+  }
+  removeBodyClass();
+
+  //Burger
+  burger.addEventListener('click', () => {
+    burger.classList.toggle('active');
+    hero.classList.toggle('active');
+    body.classList.toggle('lock');
+  });
+
+  //Catalog Cards
+  body.addEventListener('click', (e) => {
+    const target = e.target,
+          targetClass = target.className,
+          key = 'catalog__card';
+    
+    if(targetClass.indexOf(key) !== -1) {
+      console.log(e.currentTarget);
+      if (
+        target.classList.contains("catalog__card-descr") ||
+        target.classList.contains("catalog__card-link")
+      ) {
+        return;
+      }
+      removeClasses(catalogCards);
+      target.parentElement.classList.add('active');
+    } else {
+      removeClasses(catalogCards);
+    }
+  });
+
+  function removeClasses(elements) {
+    elements.forEach((el) => {
+      el.classList.remove("active");
+    });
+  }
+
+
+  const bottomFormInputs = document.querySelectorAll(".bottom__form-input");
+
+  bottomFormInputs.forEach((item) => {
+    item.addEventListener("blur", () => {
+      if (item.value != "") {
+        item.nextElementSibling.classList.add("active");
+      } else {
+        item.nextElementSibling.classList.remove("active");
+      }
+    });
+  });
+
+  //Change button text
+  if(windowWidth <= 480) {
+    galleryBtn.textContent = "Follow Me On Instagram";
+  }
+
+  //Modals
+  function modal(modal, openEl,closeEl) {
+    const open = document.querySelector(openEl),
+          close = document.querySelector(closeEl);
+
+    open.addEventListener("click", (e) => {
+      e.preventDefault();
+      modal.classList.add("active");
+      body.classList.add("lock");
+    });
+
+    close.addEventListener("click", (e) => {
+      e.preventDefault();
+      modal.classList.remove("active");
+      body.classList.remove("lock");
+    });
+  }
+
+  modal(modalAbout, ".about__btn", ".modal__about-cross img");
+
+  //hero-slider
+  const heroSlider = new Swiper(".hero__slider", {
+    direction: "horizontal",
+    loop: true,
+    pagination: {
+      el: ".hero__slider-pagination",
+      clickable: true
+    },
+    navigation: {
+      nextEl: ".hero__slider-next",
+      prevEl: ".hero__slider-prev",
+    },
+    effect: "fade",
+    fadeEffect: {
+      crossFade: true,
+    },
+    autoplay: {
+      delay: 3000
+    }
+  });
+
+  //testimonials-slider
+  const testimonialsSlider = new Swiper(".testimonials__slider", {
+    direction: "horizontal",
+    loop: true,
+    grabCursor: true,
+    keyboard: true,
+    navigation: {
+      nextEl: ".testimonials__slider-next",
+      prevEl: ".testimonials__slider-prev",
+    },
+    effect: "cube",
+    cubeEffect: {
+      shadow: false,
+      slideShadows: false,
+    },
+  });
+
+  if(windowWidth <= 992) {
+    testimonialsSlider.destroy();
+  }
+
+  window.addEventListener("click", (e) => {
+    console.log(e.target);
+  });
+});
